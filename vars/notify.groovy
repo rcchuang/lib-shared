0ad150b8fef28646ed4d00dc67bde98dad8e2261
richard chuang <richard.chuang@gsa.gov>
2021-01-15 14:27:58 -0500

    test

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,6 +1,7 @@
 import com.example.Constants
 
 def call(Map config=[:]) {
+    inputs_add_StartTime()
     if (config.type == "slack") {
         echo Constants.SLACK_MESSAGE
         echo config.message
@@ -8,4 +9,11 @@ def call(Map config=[:]) {
         echo Constants.EMAIL_MESSAGE
         echo config.message
     }
+}
+
+def inputs_add_StartTime() {
+    input_parameters_ansible= []
+    time_options = [ "0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"]
+    description_start_time = "Select the start time you wish to assign from the dropdown list"
+    input_parameters_ansible.add(choice(name: "EC2_start_time", choices: time_options, description: description_start_time))
 }
