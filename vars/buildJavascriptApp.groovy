d0032c11efc0fd8c6687a73965105bc16a56ef57
richard chuang <richard.chuang@gsa.gov>
2021-01-16 18:49:22 -0500

    test

--- vars/buildJavascriptApp.groovy
+++ vars/buildJavascriptApp.groovy
@@ -1,9 +1,6 @@
 def call(Map config=[:], Closure body) {
-    input_parameters_ansible=[] 
-    time_options = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
-    description_start_time = "Select the start time you wish to assign from the dropdown list"
-  
-    properties([parameters([choice(choices: time_options, description: description_start_time, name: 'EC2_start_time'), booleanParam(defaultValue: true, description: 'test boolean', name: 'RunningNonStop'), string(defaultValue: 'TBD', description: 'not sure', name: 'not_used', trim: false)])])
+    
+    properties([parameters([choice(choices: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'], description: 'test choice', name: 'EC2_start_time'), booleanParam(defaultValue: true, description: 'test boolean', name: 'RunningNonStop'), string(defaultValue: 'TBD', description: 'not sure', name: 'not_used', trim: false)])])
     node {
         
         stage("display values") {
@@ -27,15 +24,9 @@ def call(Map config=[:], Closure body) {
     }
 }
 
-def user_input() { 
-    input_parameters_ansible=[] 
-    time_options = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
-    description_start_time = "Select the start time you wish to assign from the dropdown list"
-    properties([parameters([choice(choices: time_options, description: description_start_time, name: 'EC2_start_time'), booleanParam(defaultValue: true, description: 'test boolean', name: 'RunningNonStop'), string(defaultValue: 'TBD', description: 'not sure', name: 'not_used', trim: false)])])
-    
-}
 def inputs_add_StartTime() {
     time_options = [ "0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"]
     description_start_time = "Select the start time you wish to assign from the dropdown list"
     input_parameters_ansible.add(choice(name: "EC2_start_time", choices: time_options, description: description_start_time))
+    echo "inputs_add_StartTime - end"
 }
