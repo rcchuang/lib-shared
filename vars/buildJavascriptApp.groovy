d97c7d0d78efda283702d2591232d062a7cd4bbf
richard chuang <richard.chuang@gsa.gov>
2021-01-16 18:47:20 -0500

    test

--- vars/buildJavascriptApp.groovy
+++ vars/buildJavascriptApp.groovy
@@ -1,6 +1,9 @@
 def call(Map config=[:], Closure body) {
+    input_parameters_ansible=[] 
+    time_options = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
+    description_start_time = "Select the start time you wish to assign from the dropdown list"
+  
     properties([parameters([choice(choices: time_options, description: description_start_time, name: 'EC2_start_time'), booleanParam(defaultValue: true, description: 'test boolean', name: 'RunningNonStop'), string(defaultValue: 'TBD', description: 'not sure', name: 'not_used', trim: false)])])
-    
     node {
         
         stage("display values") {
