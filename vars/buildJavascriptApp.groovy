d10de8cb449453ebcb35044ddae70ec7881be81a
richard chuang <richard.chuang@gsa.gov>
2021-01-16 18:05:06 -0500

    test

--- vars/buildJavascriptApp.groovy
+++ vars/buildJavascriptApp.groovy
@@ -1,4 +1,4 @@
-def call(Map config=[:], Closure body) {
+def call() {
     
     properties([parameters([choice(choices: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'], description: 'test choice', name: 'EC2_start_time'), booleanParam(defaultValue: true, description: 'test boolean', name: 'RunningNonStop'), string(defaultValue: 'TBD', description: 'not sure', name: 'not_used', trim: false)])])
     node {
@@ -9,7 +9,5 @@ def call(Map config=[:], Closure body) {
             echo "string: ${params.not_used}"
         }
 
-       
-        body()
     }
 }
