7dee82f24155575ef79ed92ddc34a1b3240abe98
richard chuang <richard.chuang@gsa.gov>
2021-01-19 16:10:42 -0500

    test

--- vars/buildJavascriptApp.groovy
+++ vars/buildJavascriptApp.groovy
@@ -1,12 +1,20 @@
 def call(Map config=[:], Closure body) {
     
-    properties([parameters([choice(choices: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'], description: 'test choice', name: 'EC2_start_time'), booleanParam(defaultValue: true, description: 'test boolean', name: 'RunningNonStop'), string(defaultValue: 'TBD', description: 'not sure', name: 'not_used', trim: false)])])
+    properties([parameters([
+      choice(choices: [' ','1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'], description: 'test choice', name: 'EC2_start_time'), 
+      choice(choices: [' ','1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'], description: 'test choice', name: 'EC2_end_time')
+      ])])
     node {
         
         stage("display values") {
             echo "choice: ${params.EC2_start_time}"
-            echo "boolean: ${params.RunningNonStop}"
-            echo "string: ${params.not_used}"
+            echo "boolean: ${params.EC2_start_time}"
+            input_parameters_ansible=[]
+            echo "size before: ${input_parameters_ansible.size()}"
+            input_parameters_ansible.add(${params.EC2_start_time})
+            input_parameters_ansible.add(${params.EC2_end_time})
+            input_parameters_ansible.add(choice(choices: [' ','1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'], description: 'test choice', name: 'EC2_start_time'))
+            echo "size after: ${input_parameters_ansible.size()}"
         }
         git url: "https://github.com/werne2j/sample-nodejs"
         stage("Install") {
